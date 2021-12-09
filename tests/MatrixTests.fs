aafeb40228c53466fb6c1e9711b4091cd8f2b556
zajer <pikus3@list.pl>
2021-12-09 22:34:57 +0100

    More tests for Matrix module.

--- tests/MatrixTests.fs
+++ tests/MatrixTests.fs
@@ -91,7 +91,20 @@ type MatrixTests () =
         let mx1 = Matrix.createUnsafe width1 height1 data1
         let mx2 = Matrix.createUnsafe width2 height2 data2
         Assert.IsFalse(mx1.isEqualTo mx2)
-        
+    [<TestMethod>]
+    member this.EqualityTest6 () = 
+        let width = 3
+        let height = 5
+        let data = [|
+            Set.ofList[0;2];
+            Set.ofList[1];
+            Set.ofList[0;1;2];
+            Set.ofList[2];
+            Set.ofList[0]
+        |]
+        let mx1 = Matrix.createUnsafe width height data
+        Assert.IsTrue(mx1.isEqualTo mx1)
+    [<TestMethod>]
     member this.CreateTest1 () =
         let rawData = [|
             [|1;1;1;1|];
@@ -101,8 +114,95 @@ type MatrixTests () =
         |]
         let width = 4
         let height = 4
-        let referenceValue = 5
-        let setOfRemovedElems = Set.ofList [1;3;4]
-        let expectedShift = 3
-        let actualShift = Utils.numberOfSmallerElements referenceValue setOfRemovedElems
-        Assert.AreEqual(expectedShift,actualShift)
+        let refData = [|
+            Set.ofList [0;1;2;3];
+            Set.ofList [2;3];
+            Set.ofList [0;1]
+            Set.empty
+        |]
+        let mx = Matrix.create height width rawData
+        let refMx = Matrix.createUnsafe height width refData
+        Assert.AreEqual(refMx,mx)
+    [<TestMethod>]
+    member this.CreateTest2 () =
+        let rawData = [|
+            [|1;1;1;1|];
+            [|0;0;1;1|];
+            [|1;1|];
+            [||]
+        |]
+        let width = 4
+        let height = 4
+        let refData = [|
+            Set.ofList [0;1;2;3];
+            Set.ofList [2;3];
+            Set.ofList [0;1]
+            Set.empty
+        |]
+        let mx = Matrix.create height width rawData
+        let refMx = Matrix.createUnsafe height width refData
+        Assert.AreEqual(refMx,mx)
+    [<TestMethod>]
+    member this.MatrixExtendingTest1 () =
+        let mx1Data = [|
+            [|0;0;1|];
+            [|0;1|];
+            [|1|];
+        |]
+        let width1 = 3
+        let height1 = 3
+        let mx2Data = [|
+            [|0;1|];
+            [|1|]
+        |]
+        let width2 = 2
+        let height2 = 2
+        let mx1 = Matrix.create height1 width1 mx1Data
+        let mx2 = Matrix.create height2 width2 mx2Data
+        let mxResData = [|
+            [|0;0;1|];
+            [|0;1|];
+            [|1|];
+            [|0;0;0;0;1|];
+            [|0;0;0;1;|]
+        |]
+        let resWidth = 5
+        let resHeight = 5
+        let expectedMx = Matrix.create resHeight resWidth mxResData
+        let resultMx = mx1.extend mx2
+        Assert.AreEqual(expectedMx,resultMx)
+    [<TestMethod>]
+    member this.MatrixExtendingTest2 () =
+        let mx1Data = [|
+            [|0;0;1|];
+            [|0;1|]
+        |]
+        let width1 = 3
+        let height1 = 2
+        let mx2Data = [|
+            [|0;0;0;1|];
+            [|0;0;1|];
+            [|0;1|];
+            [|1|];
+            [|1|]
+        |]
+        let width2 = 4
+        let height2 = 5
+        let mx1 = Matrix.create height1 width1 mx1Data
+        let mx2 = Matrix.create height2 width2 mx2Data
+        let mxResData = [|
+            [|0;0;1|];
+            [|0;1|];
+            [|0;0;0;0;0;0;1|];
+            [|0;0;0;0;0;1|];
+            [|0;0;0;0;1|];
+            [|0;0;0;1|];
+            [|0;0;0;1|]
+        |]
+        let resWidth = 7
+        let resHeight = 7
+        let expectedMx = Matrix.create resHeight resWidth mxResData
+        let resultMx = mx1.extend mx2
+        Assert.AreEqual(expectedMx,resultMx)
+
+        
